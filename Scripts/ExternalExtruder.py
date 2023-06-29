69630d72fc7162d67b190c52987ab5e3db76ed55
Michael Joyce <44496948+Mike1Joy@users.noreply.github.com>
2023-06-29 12:58:50 +0100

    Fixed

--- Scripts/ExternalExtruder.py
+++ Scripts/ExternalExtruder.py
@@ -6,6 +6,8 @@
 # For use with Cura 5.x
 # Place this script in C:\Program Files\Ultimaker Cura 5.x\share\cura\plugins\PostProcessingPlugin\scripts
 
+import math
+
 if __name__ == "__main__":
     from Debug import Script
     print("Debug mode")
@@ -25,67 +27,40 @@ class ExternalExtruder(Script):
             "version": 2,
             "settings":
             {
-                "EXRD_":
+                "EXRD_FORWARD_MAX":
                 {
-                    "label": "Extruder Change",
-                    "description": "Add GCode whenever the extruder state changes (forward, backwards or stopped).",
-                    "type": "bool",
-                    "default_value": true
+                    "label": "MaxSpeed variable",
+                    "description": "Max speed of extrusion in mm/s when motors will spin with maximum RPM - when pins are set to max (mm/s). Used in extrude bit expressions.",
+                    "type": "int",
+                    "default_value": 75
                 },
-                "EXRD_FORWARD":
+                "EXRD_FORWARD_b0":
                 {
-                    "label": "Extrude GCode",
-                    "description": "GCode to insert when extruding forward.",
+                    "label": "Extrude bit 0",
+                    "description": "Expression to decide what to set pin 0 when extruding. Use EF for current E-axis speed and MaxSpeed for maximum speed. Written in Python to be evalucated with eval().",
                     "type": "str",
-                    "default_value": "M63 P0 M63 P1; Start Extruder",
-                    "enabled": "EXRD_"
+                    "default_value": "bool(int(round(min(max(0,(EF/MaxSpeed)*14),14)))&0b0001)"
                 },
-                "EXRD_STOPPED":
+                "EXRD_FORWARD_b1":
                 {
-                    "label": "Stop Extrude GCode",
-                    "description": "GCode to insert when extruding has stopped.",
+                    "label": "Extrude bit 1",
+                    "description": "Expression to decide what to set pin 1 when extruding. Use EF for current E-axis speed and MaxSpeed for maximum speed. Written in Python to be evalucated with eval().",
                     "type": "str",
-                    "default_value": "M62 P0 M62 P1; Stop Extruder",
-                    "enabled": "EXRD_"
+                    "default_value": "bool(int(round(min(max(0,(EF/MaxSpeed)*14),14)))&0b0010)"
                 },
-                "EXRD_BACKWARDS":
+                "EXRD_FORWARD_b2":
                 {
-                    "label": "Retract GCode",
-                    "description": "GCode to insert when extruding backwards.",
+                    "label": "Extrude bit 2",
+                    "description": "Expression to decide what to set pin 2 when extruding. Use EF for current E-axis speed and MaxSpeed for maximum speed. Written in Python to be evalucated with eval().",
                     "type": "str",
-                    "default_value": "M63 P0 M63 P1; Start Extruder",
-                    "enabled": "EXRD_"
+                    "default_value": "bool(int(round(min(max(0,(EF/MaxSpeed)*14),14)))&0b0100)"
                 },
-                "GANT_":
+                "EXRD_FORWARD_b3":
                 {
-                    "label": "Gantry Move",
-                    "description": "Add GCode whenever the gantry state changes (moves or stops).",
-                    "type": "bool",
-                    "default_value": false
-                },
-                "GANT_MOVING":
-                {
-                    "label": "Gantry Moving GCode",
-                    "description": "GCode to insert when gantry is moving.",
+                    "label": "Extrude bit 3",
+                    "description": "Expression to decide what to set pin 3 when extruding. Use EF for current E-axis speed and MaxSpeed for maximum speed. Written in Python to be evalucated with eval().",
                     "type": "str",
-                    "default_value": "M62 P2; Gantry Moving",
-                    "enabled": "GANT_"
-                },
-                "GANT_STOPPED":
-                {
-                    "label": "Gantry Stopped GCode",
-                    "description": "GCode to insert when gantry is stopped.",
-                    "type": "str",
-                    "default_value": "M63 P2; Gantry Stopped",
-                    "enabled": "GANT_"
-                },
-                "AXES":
-                {
-                    "label": "Movement Axes",
-                    "description": "All axes to be treated as the gantry moving (separate with spaces).",
-                    "type": "str",
-                    "default_value": "X Y Z A B C",
-                    "enabled": "GANT_"
+                    "default_value": "bool(int(round(min(max(0,(EF/MaxSpeed)*14),14)))&0b1000)"
                 },
                 "DWELL":
                 {
@@ -93,47 +68,6 @@ class ExternalExtruder(Script):
                     "description": "Add a dwell when extruding but gantry is not moving. Eg 'G1 E1.0'",
                     "type": "bool",
                     "default_value": true
-                },
-                "DWELL_EXTR_SPEED":
-                {
-                    "label": "Stationary Feed Rate",
-                    "description": "Feed rate (mm/s) for extruder when gantry is stopped. This will determine the time the printer will dwell when extruding and not moving.",
-                    "unit": "mm/s",
-                    "type": "float",
-                    "minimum_value": "0",
-                    "default_value": 50.0,
-                    "enabled": "DWELL"
-                },
-                "DWELL_FORWARD":
-                {
-                    "label": "Dwell Forward",
-                    "description": "Dwell when extruding forward.",
-                    "type": "bool",
-                    "default_value": true,
-                    "enabled": "DWELL"
-                },
-                "DWELL_BACKWARD":
-                {
-                    "label": "Dwell Backward",
-                    "description": "Dwell when extruding backward (retracting).",
-                    "type": "bool",
-                    "default_value": false,
-                    "enabled": "DWELL"
-                },
-                "REMOVE_E":
-                {
-                    "label": "Remove E",
-                    "description": "Remove the E axis from GCode after processing.",
-                    "type": "bool",
-                    "default_value": false
-                },
-                "GCODE_FREQ":
-                {
-                    "label": "Insertion Frequency",
-                    "description": "Insert lines of GCode on every line or only when something changes.",
-                    "type": "enum",
-                    "options": {"EVERYLINE":"Every Line", "ONCHANGE":"On Change"},
-                    "default_value": "ONCHANGE"
                 },
                 "CODES":
                 {
@@ -147,65 +81,81 @@ class ExternalExtruder(Script):
                     "label": "Header GCode",
                     "description": "Header to add at top of file.",
                     "type": "str",
-                    "default_value": "M62 P0 M62 P1; Stop Extruder"
+                    "default_value": "M63 P0 M63 P1 M63 P2 M63 P3; Stop Extruder Header"
                 },
                 "FOOTER":
                 {
                     "label": "Footer GCode",
                     "description": "Footer to add at end of file.",
                     "type": "str",
-                    "default_value": "M62 P0 M62 P1; Stop Extruder"
+                    "default_value": "M63 P0 M63 P1 M63 P2 M63 P3; Stop Extruder Footer"
                 }
             }
         }"""
-
+    
     def execute(self, data):
         # Get values
-        EXRD_ = self.getSettingValueByKey("EXRD_")
-        EXRD_FORWARD = self.getSettingValueByKey("EXRD_FORWARD")
-        EXRD_STOPPED = self.getSettingValueByKey("EXRD_STOPPED")
-        EXRD_BACKWARDS = self.getSettingValueByKey("EXRD_BACKWARDS")
-        EXRD = [EXRD_FORWARD+"\n",EXRD_STOPPED+"\n",EXRD_BACKWARDS+"\n"]
-        E_FRWD = 0
-        E_STOP = 1
-        E_BKWD = 2
-        E_STATE = E_STOP
+        EXRD_FORWARD_MAX = self.getSettingValueByKey("EXRD_FORWARD_MAX")
+        MaxSpeed = int(EXRD_FORWARD_MAX)
+        EXRD_FORWARD_b0 = self.getSettingValueByKey("EXRD_FORWARD_b0")
+        EXRD_FORWARD_b1 = self.getSettingValueByKey("EXRD_FORWARD_b1")
+        EXRD_FORWARD_b2 = self.getSettingValueByKey("EXRD_FORWARD_b2")
+        EXRD_FORWARD_b3 = self.getSettingValueByKey("EXRD_FORWARD_b3")
 
-        GANT_ = self.getSettingValueByKey("GANT_")
-        GANT_MOVING = self.getSettingValueByKey("GANT_MOVING")
-        GANT_STOPPED = self.getSettingValueByKey("GANT_STOPPED")
-        GANT = [GANT_MOVING+"\n",GANT_STOPPED+"\n"]
-        G_MOVE = 0
-        G_STOP = 1
-        G_STATE = G_STOP
+        b0 = 0
+        b1 = 0
+        b2 = 0
+        b3 = 0
+
+        last_b0 = 0
+        last_b1 = 0
+        last_b2 = 0
+        last_b3 = 0
 
         DWELL = self.getSettingValueByKey("DWELL")
-        DWELL_EXTR_SPEED = self.getSettingValueByKey("DWELL_EXTR_SPEED")
-        DWELL_FORWARD = self.getSettingValueByKey("DWELL_FORWARD")
-        DWELL_BACKWARD = self.getSettingValueByKey("DWELL_BACKWARD")
-
-        REMOVE_E = self.getSettingValueByKey("REMOVE_E")
-
-        EVERYLINE = (self.getSettingValueByKey("GCODE_FREQ") == "EVERYLINE")
 
         CODES = self.getSettingValueByKey("CODES").split()
-        AXES = self.getSettingValueByKey("AXES").split()
-
         HEADER = self.getSettingValueByKey("HEADER")
         FOOTER = self.getSettingValueByKey("FOOTER")
 
         E_Absolute = False
         E_Last = 0.0
+        X_Last = 0.0
+        Y_Last = 0.0
+        Z_Last = 0.0
+
+        this_last_E = 0.0
+        this_last_X = 0.0
+        this_last_Y = 0.0
+        this_last_Z = 0.0
+
+        G0_F = 0
+        G1_F = 0
+
+        FEED = False
 
         # Go through GCode
         for layer_number, layer in enumerate(data):
             lines = layer.splitlines(keepends=True)
             for line in range(len(lines)):
-                this_E_STATE = E_STATE
-                this_G_STATE = G_STATE
+                if "G92" in lines[line]:
+                    if "E" in lines[line]:
+                        E_Last = 0
+                    if "X" in lines[line]:
+                        X_Last = 0
+                    if "Y" in lines[line]:
+                        Y_Last = 0
+                    if "Z" in lines[line]:
+                        Z_Last = 0
+                    continue
 
                 E_value = 0
+                X_value = 0
+                Y_value = 0
+                Z_value = 0
                 Dwell_time = 0
+                Gant_dist = 0
+                EF = 0
 
                 line_comment =  lines[line].split(";",maxsplit=1)
                 lines[line] = line_comment[0]
@@ -218,9 +168,16 @@ class ExternalExtruder(Script):
                 elif "M83" in lines[line]:
                     E_Absolute = False
                 
-                this_last_E = E_Last
-                
                 if any(G in lines[line] for G in CODES):
+                    FEED = "G1" in lines[line]
+
+                    if "F" in lines[line]:
+                        if FEED:
+                            G1_F = float(lines[line].split("F",maxsplit=1)[-1].split(maxsplit=1)[0])/60.0
+                        else:
+                            G0_F = float(lines[line].split("F",maxsplit=1)[-1].split(maxsplit=1)[0])/60.0
+                        
+
                     if "E" in lines[line]:
                         UpToE = lines[line].split("E",maxsplit=1)
                         EOnwards = UpToE[-1].split(maxsplit=1)
@@ -230,37 +187,86 @@ class ExternalExtruder(Script):
 
                         if E_Absolute:
                             E_value -= E_Last
-
-                        if REMOVE_E:
-                            lines[line] = UpToE[0] + ("\n" if len(EOnwards) == 1 else EOnwards[1])
-
-                    if E_value == 0:
-                        this_E_STATE = E_STOP
-                    elif E_value < 0:
-                        this_E_STATE = E_BKWD
+                    
+                    if "X" in lines[line]:
+                        this_last_X = float(lines[line].split("X",maxsplit=1)[-1].split(maxsplit=1)[0])
+                        X_value = this_last_X - X_Last
                     else:
-                        this_E_STATE = E_FRWD
-                        
-                    if any(move in lines[line] for move in AXES):
-                        this_G_STATE = G_MOVE
+                        X_value = 0.0
+                    
+                    if "Y" in lines[line]:
+                        this_last_Y = float(lines[line].split("Y",maxsplit=1)[-1].split(maxsplit=1)[0])
+                        Y_value = this_last_Y - Y_Last
                     else:
-                        this_G_STATE = G_STOP
-                        if (E_value < 0 and DWELL_BACKWARD) or (E_value > 0 and DWELL_FORWARD):
-                            Dwell_time = abs(E_value)/DWELL_EXTR_SPEED
+                        Y_value = 0.0
                     
-                    # add lines to gcode in reverse order
-                    if DWELL and this_E_STATE != E_STOP and this_G_STATE == G_STOP and Dwell_time > 0:
-                        lines[line] = f"G4 P{Dwell_time:.4f} ;Robot Dwell\n" + lines[line]
+                    if "Z" in lines[line]:
+                        this_last_Z = float(lines[line].split("Z",maxsplit=1)[-1].split(maxsplit=1)[0])
+                        Z_value = this_last_Z - Z_Last
+                    else:
+                        Z_value = 0.0
+
+                    if E_value < 0:
+                        b0 = 1
+                        b1 = 1
+                        b2 = 1
+                        b3 = 1
+                        Dwell_time = -E_value / MaxSpeed
+                    elif E_value == 0:
+                        b0 = 0
+                        b1 = 0
+                        b2 = 0
+                        b3 = 0
+                        Dwell_time = 0
+                    else:
+                        Gant_dist = math.sqrt(X_value**2 + Y_value**2 + Z_value**2)
+
+                        if Gant_dist > 0:
+                            if FEED:
+                                T = Gant_dist / G1_F
+                            else:
+                                T = Gant_dist / G0_F
+                            EF = E_value/T
+                        else:
+                            EF = MaxSpeed
+                            Dwell_time = E_value / MaxSpeed
                     
-                    if (EVERYLINE or this_E_STATE != E_STATE) and EXRD_:
-                        E_STATE = this_E_STATE
-                        lines[line] = EXRD[E_STATE] + lines[line]
+                        b0 = eval(EXRD_FORWARD_b0)
+                        b1 = eval(EXRD_FORWARD_b1)
+                        b2 = eval(EXRD_FORWARD_b2)
+                        b3 = eval(EXRD_FORWARD_b3)
+
+                    pin_string = ""
+                    if b0 != last_b0:
+                        if b0: pin_string += "M62 P0 "
+                        else:  pin_string += "M63 P0 "
+                    if b1 != last_b1:
+                        if b1: pin_string += "M62 P1 "
+                        else:  pin_string += "M63 P1 "
+                    if b2 != last_b2:
+                        if b2: pin_string += "M62 P2 "
+                        else:  pin_string += "M63 P2 "
+                    if b3 != last_b3:
+                        if b3: pin_string += "M62 P3 "
+                        else:  pin_string += "M63 P3 "
                     
-                    if (EVERYLINE or this_G_STATE != G_STATE) and GANT_:
-                        G_STATE = this_G_STATE
-                        lines[line] = GANT[G_STATE] + lines[line]
+                    #pin_string += f"; b0: {b0}, b1: {b1}, b2: {b2}, b3: {b3}, EF: {EF}, Dwell_time: {Dwell_time:.4f}, Gant_dist: {Gant_dist:.4f}, E_value: {E_value}, G1_F: {G1_F}, G0_F: {G0_F}, X_value: {X_value}, Y_value: {Y_value}, Z_value: {Z_value}"
+
+                    if len(pin_string) > 0:
+                        lines[line] = pin_string + f" ;0b{int(b3)}{int(b2)}{int(b1)}{int(b0)} = {EF}mm/s out of max {MaxSpeed}mm/s\n" + lines[line]
+                    if Dwell_time > 0 and DWELL:
+                        lines[line] = lines[line] + f"G4 P{Dwell_time:.4f} ;Robot Dwell - extrude {E_value}mm at {MaxSpeed}mm/s\n"
+                
 
                 E_Last = this_last_E
+                X_Last = this_last_X
+                Y_Last = this_last_Y
+                Z_Last = this_last_Z
+
+                last_b0 = b0
+                last_b1 = b1
+                last_b2 = b2
+                last_b3 = b3
 
                 if comment:
                     lines[line] += comment
@@ -271,24 +277,6 @@ class ExternalExtruder(Script):
         data[0] = HEADER + "\n" + data[0]
         data[-1] += FOOTER + "\n"
 
-        PrintInfo = f"""
-;GCode edited with ExternalExtruder.py script - Copyright (c) 2022 Michael Joyce-Badea
-;   EXRD_FORWARD: {EXRD_FORWARD},
-;   EXRD_STOPPED: {EXRD_STOPPED},
-;   EXRD_BACKWARDS: {EXRD_BACKWARDS},
-;   GANT_MOVING: {GANT_MOVING},
-;   GANT_STOPPED: {GANT_STOPPED},
-;   DWELL: {DWELL},
-;   DWELL_EXTR_SPEED: {DWELL_EXTR_SPEED},
-;   REMOVE_E: {REMOVE_E},
-;   GCODE_FREQ: {"EVERYLINE" if EVERYLINE else "ONCHANGE"},
-;   CODES: {CODES},
-;   AXES: {AXES},
-;   HEADER: {HEADER},
-;   FOOTER: {FOOTER}
-
-"""
-        data[0] = PrintInfo + data[0]
         return data
 
 if __name__ == "__main__":
@@ -296,19 +284,15 @@ if __name__ == "__main__":
 
     script = ExternalExtruder()
     script.KeyValue = {
-        "EXRD_FORWARD": "M62 P0 M62 P1; EXRD_FORWARD",
-        "EXRD_STOPPED": "M63 P0; EXRD_STOPPED",
-        "EXRD_BACKWARDS": "M62 P0 M63 P1; EXRD_BACKWARDS",
-        "GANT_MOVING": "M62 P2; GANT_MOVING",
-        "GANT_STOPPED": "M63 P2; GANT_STOPPED",
+        "EXRD_FORWARD_MAX": 75,
+        "EXRD_FORWARD_b0": "bool(int(round(min(max(0,(EF/MaxSpeed)*14),14)))&0b0001)",
+        "EXRD_FORWARD_b1": "bool(int(round(min(max(0,(EF/MaxSpeed)*14),14)))&0b0010)",
+        "EXRD_FORWARD_b2": "bool(int(round(min(max(0,(EF/MaxSpeed)*14),14)))&0b0100)",
+        "EXRD_FORWARD_b3": "bool(int(round(min(max(0,(EF/MaxSpeed)*14),14)))&0b1000)",
         "DWELL": True,
-        "DWELL_EXTR_SPEED": 15.0,
-        "REMOVE_E": False,
-        "GCODE_FREQ": "ONCHANGE",
         "CODES": "G0 G1 G2 G3 G4 G28",
-        "AXES": "X Y Z A B C",
-        "HEADER": "M63 P0 M63 P1 M63 P2 ;Turn all pins off",
-        "FOOTER": "M63 P0 M63 P1 M63 P2 ;Turn all pins off"
+        "HEADER": "M63 P0 M63 P1 M63 P2 M63 P3; Stop Extruder Header",
+        "FOOTER": "M63 P0 M63 P1 M63 P2 M63 P3; Stop Extruder Footer"
     }
     script.DEBUG()
 
